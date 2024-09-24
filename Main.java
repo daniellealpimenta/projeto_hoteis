38f24881ec9faae81c9d15cb8fe8e5f62c506a73
Daniel Leal Pimenta <129066504+daniellealpimenta@users.noreply.github.com>
2024-09-24 11:15:33 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
-import CONEXAODB.DB;
+package CONEXAODB;
+
 import CRUD.CRUD;
 
 import java.util.Scanner;
 
 public class Main {
+
+    static boolean ligado = true;
+    static int opcao;
+
     public static void main(String[] args) {
         CRUD crud = new CRUD();
         DB conection = new DB();
@@ -12,12 +17,12 @@ public class Main {
 
         Scanner ler = new Scanner(System.in);
 
-        while (crud.isLigado()) {
+        while (ligado) {
 
             System.out.println("""
                     
                     ------ MENU DE OPÇÕES ------
-                    
+
                     1 - ADICIONAR
                     2 - REMOVER
                     3 - EDITAR
@@ -26,9 +31,10 @@ public class Main {
                     
                     """);
 
-            crud.setOpcao(ler.nextInt());
+            opcao = ler.nextInt();
+            ler.nextLine(); // Consumir o '\n' após o nextInt()
 
-            switch (crud.getOpcao()) {
+            switch (opcao) {
 
                 case 1:
                     System.out.println("""
@@ -44,115 +50,82 @@ public class Main {
                             5 - INFORMACAO RESERVA
                             
                             """);
-                    crud.setOpcao(ler.nextInt());
+                    opcao = ler.nextInt();
+                    ler.nextLine(); // Consumir o '\n' após o nextInt()
 
-                    if (crud.getOpcao() == 1) {
+                    if (opcao == 1) {
 
                         crud.setTabela("cliente");
                         System.out.println("""
                                 ------ ATRIBUTOS ------
                                 
-                                -nome
-                                -telefone_contato
-                                -cpf
-                                -endereco
-                                -idade
-                                -email
-                             
-                                ESCREVA QUAIS ATRIBUTOS VOCÊ DESEJA ACRESCENTAR:
+                                nome, telefone_contato, cpf, endereco, idade, email
+                                
+                                ESCREVA OS VALORES DOS ATRIBUTOS NA ORDEM ACIMA:
                                 """);
-                        ler.nextLine();
-                        crud.setAtributo(ler.nextLine());
-
-                        System.out.println("QUAL OS VALORES DOS ATRIBUTOS SELECIONADOS? (ESCREVA)");
-                        crud.setValor(ler.nextLine());
-
+                        String valores = ler.nextLine();
+                        crud.setValor(valores);
                         crud.insert();
 
-                    } else if (crud.getOpcao() == 2) {
+                    } else if (opcao == 2) {
 
                         crud.setTabela("quarto");
                         System.out.println("""
                                 ------ ATRIBUTOS ------
                                 
-                                -numero
-                                -capacidade
-                                -classificacao
-                                -preco
-                                -categoria
-                             
-                                ESCREVA QUAIS ATRIBUTOS VOCÊ DESEJA ACRESCENTAR:
+                                numero, capacidade, classificacao, preco, categoria
+                                
+                                ESCREVA OS VALORES DOS ATRIBUTOS NA ORDEM ACIMA:
                                 """);
-                        ler.nextLine();
-                        crud.setAtributo(ler.nextLine());
-
-                        System.out.println("QUAL OS VALORES DOS ATRIBUTOS SELECIONADOS? (ESCREVA)");
-                        crud.setValor(ler.nextLine());
-
+                        String valores = ler.nextLine();
+                        crud.setValor(valores);
                         crud.insert();
 
-                    } else if (crud.getOpcao() == 3) {
+                    } else if (opcao == 3) {
 
                         crud.setTabela("funcionario");
                         System.out.println("""
                                 ------ ATRIBUTOS ------
                                 
-                                -nome
-                                -cargo
-                                -salario
-                                -datadenasc
-                             
-                                ESCREVA QUAIS ATRIBUTOS VOCÊ DESEJA ACRESCENTAR:
+                                nome, cargo, salario, datadenasc
+                                
+                                ESCREVA OS VALORES DOS ATRIBUTOS NA ORDEM ACIMA:
                                 """);
-                        ler.nextLine();
-                        crud.setAtributo(ler.nextLine());
-
-                        System.out.println("QUAL OS VALORES DOS ATRIBUTOS SELECIONADOS? (ESCREVA)");
-                        crud.setValor(ler.nextLine());
-
+                        String valores = ler.nextLine();
+                        crud.setValor(valores);
                         crud.insert();
 
-                    } else if (crud.getOpcao() == 4) {
+                    } else if (opcao == 4) {
 
                         crud.setTabela("servico_funcionario");
                         System.out.println("""
                                 ------ ATRIBUTOS ------
                                 
-                                -duracao_servico
-                                -tipo_servico
-                             
-                                ESCREVA QUAIS ATRIBUTOS VOCÊ DESEJA ACRESCENTAR:
+                                duracao_servico, tipo_servico
+                                
+                                ESCREVA OS VALORES DOS ATRIBUTOS NA ORDEM ACIMA:
                                 """);
-                        ler.nextLine();
-                        crud.setAtributo(ler.nextLine());
-
-                        System.out.println("QUAL OS VALORES DOS ATRIBUTOS SELECIONADOS? (ESCREVA)");
-                        crud.setValor(ler.nextLine());
-
+                        String valores = ler.nextLine();
+                        crud.setValor(valores);
                         crud.insert();
 
-                    } else if (crud.getOpcao() == 5) {
+                    } else if (opcao == 5) {
 
                         crud.setTabela("informacao_reserva");
                         System.out.println("""
                                 ------ ATRIBUTOS ------
                                 
-                                -data_inicio_reserva
-                                -data_fim_reserva
-                                -valor
-                             
-                                ESCREVA QUAIS ATRIBUTOS VOCÊ DESEJA ACRESCENTAR:
+                                data_inicio_reserva, data_fim_reserva, valor
+                                
+                                ESCREVA OS VALORES DOS ATRIBUTOS NA ORDEM ACIMA:
                                 """);
-                        ler.nextLine();
-                        crud.setAtributo(ler.nextLine());
-
-                        System.out.println("QUAL OS VALORES DOS ATRIBUTOS SELECIONADOS? (ESCREVA)");
-                        crud.setValor(ler.nextLine());
-
+                        String valores = ler.nextLine();
+                        crud.setValor(valores);
                         crud.insert();
 
                     }
                     break;
+
                 case 2:
                     System.out.println("""
                             
@@ -167,52 +140,36 @@ public class Main {
                             5 - INFORMACAO RESERVA
                             
                             """);
-                    crud.setOpcao(ler.nextInt());
+                    opcao = ler.nextInt();
+                    ler.nextLine(); // Consumir o '\n' após o nextInt()
 
-                    int id;
+                    System.out.println("QUAL O ID DO VALOR QUE VOCÊ GOSTARIA DE REMOVER?");
+                    int id = ler.nextInt();
+                    ler.nextLine(); // Consumir o '\n' após o nextInt()
 
-                    if (crud.getOpcao() == 1) {
+                    if (opcao == 1) {
                         crud.setTabela("cliente");
-                        System.out.println("QUAL O ID DO VALOR QUE VOCÊ GOSTARIA DE REMOVER?");
-                        ler.nextLine();
-                        id = ler.nextInt();
-
                         crud.drop(id);
 
-                    } else if (crud.getOpcao() == 2) {
+                    } else if (opcao == 2) {
                         crud.setTabela("quarto");
-                        System.out.println("QUAL O ID DO VALOR QUE VOCÊ GOSTARIA DE REMOVER?");
-                        ler.nextLine();
-                        id = ler.nextInt();
-
                         crud.drop(id);
 
-                    } else if (crud.getOpcao() == 3) {
+                    } else if (opcao == 3) {
                         crud.setTabela("funcionario");
-                        System.out.println("QUAL O ID DO VALOR QUE VOCÊ GOSTARIA DE REMOVER?");
-                        ler.nextLine();
-                        id = ler.nextInt();
-
                         crud.drop(id);
 
-                    } else if (crud.getOpcao() == 4) {
+                    } else if (opcao == 4) {
                         crud.setTabela("servico_funcionario");
-                        System.out.println("QUAL O ID DO VALOR QUE VOCÊ GOSTARIA DE REMOVER?");
-                        ler.nextLine();
-                        id = ler.nextInt();
-
                         crud.drop(id);
 
-                    } else if (crud.getOpcao() == 5) {
+                    } else if (opcao == 5) {
                         crud.setTabela("informacao_reserva");
-                        System.out.println("QUAL O ID DO VALOR QUE VOCÊ GOSTARIA DE REMOVER?");
-                        ler.nextLine();
-                        id = ler.nextInt();
-
                         crud.drop(id);
                     }
 
                     break;
+
                 case 3:
                     System.out.println("""
                             
@@ -227,60 +184,47 @@ public class Main {
                             5 - INFORMACAO RESERVA
                             
                             """);
-                    crud.setOpcao(ler.nextInt());
-                    if (crud.getOpcao() == 1) {
+                    opcao = ler.nextInt();
+                    ler.nextLine(); // Consumir o '\n' após o nextInt()
 
+                    System.out.println("QUAL O VALOR ANTIGO QUE VOCÊ GOSTARIA DE ALTERAR?");
+                    String valorAntigo = ler.nextLine();
+                    System.out.println("QUAL O NOVO VALOR?");
+                    String valorNovo = ler.nextLine();
+
+                    if (opcao == 1) {
                         crud.setTabela("cliente");
-                        System.out.println("QUAL O ATRIBUTO ANTIGO QUE VOCÊ GOSTARIA DE ALTERAR? (ESCREVA)");
-                        ler.nextLine();
-                        crud.setValorAntigo(ler.nextLine());
-                        System.out.println("QUAL O ATRIBUTO NOVO QUE VOCÊ GOSTARIA DE COLOCAR? (ESCREVA)");
-                        crud.setValorNovo(ler.nextLine());
-
+                        crud.setValorAntigo(valorAntigo);
+                        crud.setValorNovo(valorNovo);
                         crud.update();
 
-                    } else if (crud.getOpcao() == 2) {
+                    } else if (opcao == 2) {
                         crud.setTabela("quarto");
-                        System.out.println("QUAL O ATRIBUTO ANTIGO QUE VOCÊ GOSTARIA DE ALTERAR? (ESCREVA)");
-                        ler.nextLine();
-                        crud.setValorAntigo(ler.nextLine());
-                        System.out.println("QUAL O ATRIBUTO NOVO QUE VOCÊ GOSTARIA DE COLOCAR? (ESCREVA)");
-                        crud.setValorNovo(ler.nextLine());
-
+                        crud.setValorAntigo(valorAntigo);
+                        crud.setValorNovo(valorNovo);
                         crud.update();
 
-                    } else if (crud.getOpcao() == 3) {
+                    } else if (opcao == 3) {
                         crud.setTabela("funcionario");
-                        System.out.println("QUAL O ATRIBUTO ANTIGO QUE VOCÊ GOSTARIA DE ALTERAR? (ESCREVA)");
-                        ler.nextLine();
-                        crud.setValorAntigo(ler.nextLine());
-                        System.out.println("QUAL O ATRIBUTO NOVO QUE VOCÊ GOSTARIA DE COLOCAR? (ESCREVA)");
-                        crud.setValorNovo(ler.nextLine());
-
+                        crud.setValorAntigo(valorAntigo);
+                        crud.setValorNovo(valorNovo);
                         crud.update();
 
-                    } else if (crud.getOpcao() == 4) {
+                    } else if (opcao == 4) {
                         crud.setTabela("servico_funcionario");
-                        System.out.println("QUAL O ATRIBUTO ANTIGO QUE VOCÊ GOSTARIA DE ALTERAR? (ESCREVA)");
-                        ler.nextLine();
-                        crud.setValorAntigo(ler.nextLine());
-                        System.out.println("QUAL O ATRIBUTO NOVO QUE VOCÊ GOSTARIA DE COLOCAR? (ESCREVA)");
-                        crud.setValorNovo(ler.nextLine());
-
+                        crud.setValorAntigo(valorAntigo);
+                        crud.setValorNovo(valorNovo);
                         crud.update();
 
-                    } else if (crud.getOpcao() == 5) {
+                    } else if (opcao == 5) {
                         crud.setTabela("informacao_reserva");
-                        System.out.println("QUAL O ATRIBUTO ANTIGO QUE VOCÊ GOSTARIA DE ALTERAR? (ESCREVA)");
-                        ler.nextLine();
-                        crud.setValorAntigo(ler.nextLine());
-                        System.out.println("QUAL O ATRIBUTO NOVO QUE VOCÊ GOSTARIA DE COLOCAR? (ESCREVA)");
-                        crud.setValorNovo(ler.nextLine());
-
+                        crud.setValorAntigo(valorAntigo);
+                        crud.setValorNovo(valorNovo);
                         crud.update();
-
                     }
+
                     break;
+
                 case 4:
                     System.out.println("""
                             
@@ -296,45 +240,47 @@ public class Main {
                             6 - INFORMACAO RESERVA
                             
                             """);
-                    crud.setOpcao(ler.nextInt());
+                    opcao = ler.nextInt();
+                    ler.nextLine(); // Consumir o '\n' após o nextInt()
 
-                    if (crud.getOpcao() == 1) {
+                    if (opcao == 1) {
                         crud.setTabela("cliente");
-
                         crud.select();
 
-                    } else if (crud.getOpcao() == 2) {
+                    } else if (opcao == 2) {
                         crud.setTabela("quarto");
-
                         crud.select();
 
-                    } else if (crud.getOpcao() == 3) {
+                    } else if (opcao == 3) {
                         crud.setTabela("funcionario");
-
                         crud.select();
 
-                    } else if (crud.getOpcao() == 4) {
+                    } else if (opcao == 4) {
                         crud.setTabela("cartao_acesso");
-
                         crud.select();
 
-                    } else if (crud.getOpcao() == 5) {
+                    } else if (opcao == 5) {
                         crud.setTabela("servico_funcionario");
-
                         crud.select();
 
-                    } else if (crud.getOpcao() == 6) {
+                    } else if (opcao == 6) {
                         crud.setTabela("informacao_reserva");
-
                         crud.select();
-
                     }
+
                     break;
+
                 case 5:
-                    System.exit(0);
-                    crud.desligar();
+                    System.out.println("Saindo do sistema...");
+                    ligado = false;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
                     break;
             }
         }
+
+        ler.close();
     }
 }
